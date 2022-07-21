bf70f438f59f9fc67578abcc67427d70ca23b435
GSvenny <gabrielswagner7@gmail.com>
2022-07-21 02:07:21 -0500

    Update Employee Role complete

--- server.js
+++ server.js
@@ -10,14 +10,15 @@
 
 //View Departments:
     //formated table showing department names and deparment ids
-
 //----COMPLETE---//
 
 //View all Roles
     //Presented with job title, role id, the department that role belongs to, and the ssalary for that role
+//----COMPLETE---//
 
 //view all employees
     //Presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the eployees report to
+//____COMPLETE---//
 
 //Add a department
     //Prompted to enter the name of the deparment and that department is added to the database
@@ -25,12 +26,13 @@
 
 //Add a Role
     //Prompted to enter the name, salary, and deparment for the role and that role is added to the database
-
+//----COMPLETE---/
 //Add an employee
     //Prompted to enter the employee's first name, last name, role, and manager, and that employee is added to the database
-
+//---COMPLETE---/
 //Update an employee role
     //Prompted to select an employee to update and their new role and this information is updated in the database. 
+//---COMPLETE---/
 
 //BONUS
     //Update Employee managers
@@ -202,6 +204,30 @@ function addEmployee() {
         });
     });
 }
+
+function updateEmployeeRole() {
+    db.query('SELECT * FROM employee', (err, res) => {
+    console.log(res);
+    })
+    inquirer.prompt([
+        {   
+            type: 'input',
+            message: "Please enter the employee's ID number that you wish to update",
+            name: "employee_id"
+        },
+        {
+            type: "number",
+            message: "Please enter their new ID number",
+            name: "roleId"
+        }
+    ]).then (res => {
+        db.query('UPDATE employee SET role_id = ? WHERE id = ?', [res.roleId, res.employee_id], (err, res) => {
+            if (err) throw err;
+            console.log("Employee's Role updated")
+            menu()
+        })
+    })
+};
 
 function deleteDepartment() {
     console.log
